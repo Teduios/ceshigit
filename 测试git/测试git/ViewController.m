05c675e1ef8a12dbf74f7dc14ba9504f105549a0
GUOAJ <guoaj@guodenghongdemacbook-pro.local>
2016-07-29 19:42:24 +0800

    cash version3

--- 测试git/测试git/ViewController.m
+++ 测试git/测试git/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"version1");
+    NSLog(@"version3");
 }
 
 - (void)didReceiveMemoryWarning {
